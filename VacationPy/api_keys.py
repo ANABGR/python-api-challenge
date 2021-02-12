f3a3ba14e3e7cb062ce32ba19b6100759021749f
ANABGR <anabgr1@gmail.com>
2021-02-12 00:12:24 -0600

    final push

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "a62dcadb531713ddb58e9846fe4fb73c"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCXMCDEdbSjOUDzHYtGZznU8uGPhmjRj7U"
